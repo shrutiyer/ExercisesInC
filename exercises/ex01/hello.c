2f293c9fd9302718b4c678aa87e1aafd9b070a41
shrutiyer <shruti.iyer@students.olin.edu>
2018-01-26 12:01:02 -0500

    Finished ex01 during Lecture 2

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+void print_c(int c) {
+  int b = (c%2 == 0);
+  switch (b) {
+    case 0:
+        printf("c is odd\n");
+        break;
+    case 1:
+        printf("c is even\n");
+        break;
+    }
+}
+
 int main() {
     printf("Hello, World!\n");
+    int a = 3;
+    int b = 4;
+    int c = a + b;
+    printf("c is %d\n", c);
+    print_c(c);
     return 0;
 }
---
+++ exercises/ex01/hello.s
@@ -0,0 +1,90 @@
+	.file	"hello.c"
+	.section	.rodata
+.LC0:
+	.string	"c is odd"
+.LC1:
+	.string	"c is even"
+	.text
+	.globl	print_c
+	.type	print_c, @function
+print_c:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movl	-20(%rbp), %eax
+	andl	$1, %eax
+	testl	%eax, %eax
+	sete	%al
+	movzbl	%al, %eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	testl	%eax, %eax
+	je	.L3
+	cmpl	$1, %eax
+	je	.L4
+	jmp	.L5
+.L3:
+	movl	$.LC0, %edi
+	call	puts
+	jmp	.L2
+.L4:
+	movl	$.LC1, %edi
+	call	puts
+	nop
+.L2:
+.L5:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	print_c, .-print_c
+	.section	.rodata
+.LC2:
+	.string	"Hello, World!"
+.LC3:
+	.string	"c is %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$.LC2, %edi
+	call	puts
+	movl	$3, -12(%rbp)
+	movl	$4, -8(%rbp)
+	movl	-12(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC3, %edi
+	movl	$0, %eax
+	call	printf
+	movl	-4(%rbp), %eax
+	movl	%eax, %edi
+	call	print_c
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
+	.section	.note.GNU-stack,"",@progbits
