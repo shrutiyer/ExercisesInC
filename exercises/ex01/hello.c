45751012802e78de4a3d62da7a953f33016785c8
shrutiyer <shruti.iyer@students.olin.edu>
2018-01-29 08:36:29 -0500

    Finished the hello.c exercises and corresponding questions

--- exercises/ex01/hello.c
+++ exercises/ex01/hello.c
@@ -1,23 +1,27 @@
 #include <stdio.h>
 
-void print_c(int c) {
-  int b = (c%2 == 0);
-  switch (b) {
-    case 0:
-        printf("c is odd\n");
-        break;
-    case 1:
-        printf("c is even\n");
-        break;
-    }
-}
+/*    In the non-optimized version, the value of the variables can be seen even
+      though it is never going to be used whereas in the optimized version, the
+      compilier probably spots the unused variables and removes them.
+
+      But, when x is printed, the optimized version does save the value in a
+      register since it will be used later on for printing. In both this case
+      and the one above, there are almost half number of operations (movl,
+      subq, addq, xorl) made in the optimized version when compared to the non
+      optimized version.
+
+      When the value of y is calculated using x and 1, and y is printed, the
+      optimization removes unnecessary steps that the non-optimized version
+      performs, automatically calculates the value of y as 6 and prints it.
+
+      In all three cases, there were almost half number of operations (movl,
+      subq, addq, xorl) made in the optimized version when compared to the
+      non-optimized version.
+*/
 
 int main() {
-    printf("Hello, World!\n");
-    int a = 3;
-    int b = 4;
-    int c = a + b;
-    printf("c is %d\n", c);
-    print_c(c);
+    int x = 5;
+    int y = x + 1;
+    printf("Value of y %i\n", y);
     return 0;
 }
--- exercises/ex01/hello.s
+++ exercises/ex01/hello.s
@@ -1,90 +1,34 @@
 	.file	"hello.c"
-	.section	.rodata
+	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
-	.string	"c is odd"
-.LC1:
-	.string	"c is even"
-	.text
-	.globl	print_c
-	.type	print_c, @function
-print_c:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$32, %rsp
-	movl	%edi, -20(%rbp)
-	movl	-20(%rbp), %eax
-	andl	$1, %eax
-	testl	%eax, %eax
-	sete	%al
-	movzbl	%al, %eax
-	movl	%eax, -4(%rbp)
-	movl	-4(%rbp), %eax
-	testl	%eax, %eax
-	je	.L3
-	cmpl	$1, %eax
-	je	.L4
-	jmp	.L5
-.L3:
-	movl	$.LC0, %edi
-	call	puts
-	jmp	.L2
-.L4:
-	movl	$.LC1, %edi
-	call	puts
-	nop
-.L2:
-.L5:
-	nop
-	leave
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	print_c, .-print_c
-	.section	.rodata
-.LC2:
-	.string	"Hello, World!"
-.LC3:
-	.string	"c is %d\n"
-	.text
+	.string	"Value of y %i\n"
+	.section	.text.unlikely,"ax",@progbits
+.LCOLDB1:
+	.section	.text.startup,"ax",@progbits
+.LHOTB1:
+	.p2align 4,,15
 	.globl	main
 	.type	main, @function
 main:
-.LFB1:
+.LFB23:
 	.cfi_startproc
-	pushq	%rbp
+	subq	$8, %rsp
 	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movl	$.LC2, %edi
-	call	puts
-	movl	$3, -12(%rbp)
-	movl	$4, -8(%rbp)
-	movl	-12(%rbp), %edx
-	movl	-8(%rbp), %eax
-	addl	%edx, %eax
-	movl	%eax, -4(%rbp)
-	movl	-4(%rbp), %eax
-	movl	%eax, %esi
-	movl	$.LC3, %edi
-	movl	$0, %eax
-	call	printf
-	movl	-4(%rbp), %eax
-	movl	%eax, %edi
-	call	print_c
-	movl	$0, %eax
-	leave
-	.cfi_def_cfa 7, 8
+	movl	$6, %edx
+	movl	$.LC0, %esi
+	movl	$1, %edi
+	xorl	%eax, %eax
+	call	__printf_chk
+	xorl	%eax, %eax
+	addq	$8, %rsp
+	.cfi_def_cfa_offset 8
 	ret
 	.cfi_endproc
-.LFE1:
+.LFE23:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.4) 5.4.0 20160609"
+	.section	.text.unlikely
+.LCOLDE1:
+	.section	.text.startup
+.LHOTE1:
+	.ident	"GCC: (Ubuntu 5.4.0-6ubuntu1~16.04.5) 5.4.0 20160609"
 	.section	.note.GNU-stack,"",@progbits
