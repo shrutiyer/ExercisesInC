bcedfa4bc4a965e43fb9f86811ba934a0937e654
shrutiyer <shruti.iyer@students.olin.edu>
2018-02-18 15:07:10 -0500

    Finished debugging and refactoring tee.c

--- exercises/ex03/tee.c
+++ exercises/ex03/tee.c
@@ -7,22 +7,26 @@
  *  I have never used or even heard about the command `tee`. So the initial
  *  hurdle was to learn how the command can be used. Once, I understood that,
  *  the problem decomposition became really easy. The overall implementation of
- *  the program required a bit of searching online and a little bit of dec
+ *  the program required a bit of searching online and a little bit of debugging.
+
+ * 
 */
 
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
 
-void main(int argc, char **argv[]) {
+void main(int argc, char *argv[]) {
     FILE *file_read;
     FILE *file_write[argc];
     char buffer;
-    char file_write_type;
+
+    int opt;
     int append = 0;
 
     // Check for the appropriate number of arguments
-    if(argc <= 3){
+    if (argc < 2){
         printf("Incorrect number of arguments given to the command");
         exit(0);
     }
@@ -32,34 +36,35 @@ void main(int argc, char **argv[]) {
         append = 1;
     }
 
-    if (append) {
-        char file_write_type = "a";
-    } else {
-        char file_write_type = "w";
+    // Read from stdin
+    file_read = stdin;
+    if (file_read == NULL) {
+        printf("Standard Input is NULL");
+        exit(0);
     }
 
-    // TODO: Check if stdin is null
-
-    // Read from stdin
-    file_read = fopen(*argv[1+append], "r");
     // Create new files according to the number that user inputed
-    for (int file_count=2+append; file_count<argc; file_count++) {
-        file_write[file_count] = fopen(*argv[file_count], file_write_type);
+    for (int file_count=1+append; file_count<argc; file_count++) {
+        if (append) {
+            file_write[file_count] = fopen(argv[file_count], "a");
+        } else {
+            file_write[file_count] = fopen(argv[file_count], "w");
+        }
     }
 
     // Continue reading until end of file for stdin
     while (!feof(file_read)) {
-        buffer = fgetc(file_read);
+        buffer = getc(file_read);
         printf("%c", buffer);
         // Write the stdout and to all the files
-        for (int file_count=2+append; file_count<argc; file_count++) {
+        for (int file_count=1+append; file_count<argc; file_count++) {
             fputc(buffer, file_write[file_count]);
         }
     }
 
     // Close all files
     fclose(file_read);
-    for (int file_count=2+append; file_count<argc; file_count++) {
+    for (int file_count=1+append; file_count<argc; file_count++) {
         fclose(file_write[file_count]);
     }
 }
