1a2fac6793558b9fb0599970a987c7a55e58feed
shrutiyer <shruti.iyer@students.olin.edu>
2018-02-18 13:19:34 -0500

    Now tee takes in multiple files

--- exercises/ex03/tee.c
+++ exercises/ex03/tee.c
@@ -7,7 +7,7 @@
  *  I have never used or even heard about the command `tee`. So the initial
  *  hurdle was to learn how the command can be used. Once, I understood that,
  *  the problem decomposition became really easy. The overall implementation of
- *  the program required a bit of searching online and a little bit of debugging.
+ *  the program required a bit of searching online and a little bit of dec
 */
 
 #include <stdio.h>
@@ -16,28 +16,37 @@
 
 void main(int argc, char **argv[]) {
     FILE *file_read;
-    FILE *file_write;
+    FILE *file_write[argc];
     char buffer;
 
     // Check for the appropriate number of arguments
-    if(argc != 3){
+    if(argc <= 3){
         printf("Incorrect number of arguments given to the command");
         exit(0);
     }
 
     // TODO: Check if stdin is null
 
-
-
+    // Read from stdin
     file_read = fopen(*argv[1], "r");
-    file_write = fopen(*argv[2], "w");
+    // Create new files according to the number that user inputed
+    for (int file_count=2; file_count<argc; file_count++) {
+        file_write[file_count] = fopen(*argv[file_count], "w");
+    }
 
+    // Continue reading until end of file for stdin
     while (!feof(file_read)) {
         buffer = fgetc(file_read);
         printf("%c", buffer);
-        fputc(buffer, file_write);
+        // Write the stdout and to all the files
+        for (int file_count=2; file_count<argc; file_count++) {
+            fputc(buffer, file_write[file_count]);
+        }
     }
 
+    // Close all files
     fclose(file_read);
-    fclose(file_write);
+    for (int file_count=2; file_count<argc; file_count++) {
+        fclose(file_write[file_count]);
+    }
 }
