a08631fb33272733b76a3f4e1609955d6fb9e562
shrutiyer <shruti.iyer@students.olin.edu>
2018-02-25 20:23:36 -0500

    finished part 1 of hw04

---
+++ exercises/ex04/trout/trout.h
@@ -0,0 +1,44 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <time.h>
+#include <netinet/in.h>
+#include <netinet/in_systm.h>
+#include <netinet/ip.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/udp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <sys/un.h>
+#include <stdarg.h>
+#include <syslog.h>
+#include "util.h"
+
+#define BUFSIZE 1500
+
+typedef struct timeval Timeval;
+typedef struct sockaddr Sockaddr;
+
+/* the following are a few definitions from Stevens' unp.h */
+
+typedef	void Sigfunc(int);        /* for signal handlers */
+
+typedef struct rec {                /* outgoing UDP data */
+  u_short seq;          /* sequence number */
+} Rec;
+
+#define max(a,b) ((a) > (b) ? (a) : (b))
+
+extern int datalen;
+extern int max_ttl;
+extern Sockaddr *sasend;
+extern Sockaddr *sarecv;
+extern Sockaddr *salast;
+extern Sockaddr *sabind;
+extern socklen_t salen;
